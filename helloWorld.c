22b37d429063f525e956d731d7b7c7ae9e1e8100
Ahmet149 <145220672+Ahmet149@users.noreply.github.com>
2023-09-16 20:35:03 +0300

    Add files via upload

--- helloWorld.c
+++ helloWorld.c
@@ -10,7 +10,22 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main ()
 {
-    printf("Hello World");
+    int x,y,t,c,ca,b;
+    printf("1.sayiyi giriniz: ");
+    scanf("%d",&x);
+    printf("2.sayiyi giriniz: ");
+    scanf("%d",&y);
 
+    t=x+y;
+    c=x-y;
+    ca=x*y;
+    b=x/y;
+
+    printf("Toplam sonucu: %d\n",t);
+    printf("Fark sonucu: %d\n",c);
+    print("Carpim sonucu: %d\n",ca);
+    printf("Bolumun sonucu: %d",b);
+
+    
     return 0;
 }
